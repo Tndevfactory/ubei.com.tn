cd2d66a8ee6e9c0dd2d940f8c456858976630b41
Tndevfactory <tndevfactory@gmail.com>
2022-04-28 04:28:43 +0100

    h

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,8 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import MuiLink from "@mui/material/Link";
 import { Container } from "@mui/material";
-
+import { FacebookShareButton, FacebookIcon } from "next-share";
+import { PinterestShareButton, PinterestIcon } from "next-share";
 import dynamic from "next/dynamic";
 
 const Facebook = dynamic(() => import("./Facebook"));
@@ -22,6 +23,22 @@ export default function Navbar() {
     >
       <Container maxWidth="xl">
         {/* <Facebook /> */}
+        <FacebookShareButton
+          url={"https://www.tndev-art.tn/"}
+          quote={"share java app"}
+          hashtag={"#nextshare"}
+        >
+          <FacebookIcon size={32} round />
+        </FacebookShareButton>
+
+        <PinterestShareButton
+          url={"www.tndev-art.tn"}
+          media={
+            "next-share is a social share buttons for your next React apps."
+          }
+        >
+          <PinterestIcon size={32} round />
+        </PinterestShareButton>
         <Typography variant="body2" color="secondary" align="center">
           {"Copyright © "}
           <MuiLink color="inherit" href="https://tndev-art.com">
